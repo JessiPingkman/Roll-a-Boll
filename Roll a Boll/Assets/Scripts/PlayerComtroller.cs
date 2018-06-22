c542d8607f76984d447dd10882b29f5e19642515
Nikita <jokerxxx99291488@gmail.com>
2018-06-22 13:55:42 +0600

    Ready to beta version of the game

--- Roll a Boll/Assets/Scripts/PlayerComtroller.cs
+++ Roll a Boll/Assets/Scripts/PlayerComtroller.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerComtroller : MonoBehaviour {
-    public float speed;
+
+    public float speed = 6f;
     public Rigidbody rb;
+    public Text CountText, WinText;
+    private int count;
+
     void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCount();
     }
 
     void FixedUpdate() {
@@ -15,5 +22,19 @@ public class PlayerComtroller : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Cube") {
+            Destroy(other.gameObject);
+            count++;
+            SetCount();
+        }
+    }
+    void SetCount() {
+        CountText.text = "Count: " + count.ToString();
+        if(count >= 13) {
+            WinText.text = "YOU WIN!";
+        }
     }
 }
