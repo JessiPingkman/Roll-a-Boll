c4947e9c7d3d9bed35391a0f3047ed791acfa791
Nikita <jokerxxx99291488@gmail.com>
2018-08-10 21:22:32 +0600

    this shit finally works

---
+++ New AR game/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+    private float speed = 4f;
+    private Animation anim;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+	}
+
+    void Update() {
+        float x = CrossPlatformInputManager.GetAxis("Vertical");
+        float y = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(x, 0, y);
+        rb.velocity = movement * speed;
+
+        if (x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+
+        if (x != 0 || y != 0)
+        {
+            anim.Play("Walk");
+        }
+        else
+        {
+            anim.Play("Idel");
+        }
+	}
+}
